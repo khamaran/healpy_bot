4e0679013ae755df6ad644634777bfdabd878f90
Мария Тумилович <khamaran@yahoo.com>
2023-04-23 18:03:28 +0300

    Сделаны картинки и погода

--- config_reader.py
+++ config_reader.py
@@ -1,8 +1,19 @@
+import os
+
 from pydantic import BaseSettings, SecretStr
+from dotenv import load_dotenv
+load_dotenv()
+
+weather_key = os.getenv('WEATHER_API_KEY')
 
 
 class Settings(BaseSettings):
     bot_token: SecretStr
+    CURRENT_WEATHER_API_CALL = (
+            'https://api.openweathermap.org/data/2.5/weather?'
+            'lat={latitude}&lon={longitude}&'
+            'appid=' + weather_key + '&units=metric'
+    )
 
     class Config:
         # Имя файла, откуда будут прочитаны данные
--- healpy.py
+++ healpy.py
@@ -1,12 +1,14 @@
+import asyncio
+import logging
+import requests
+
 from aiogram.dispatcher.filters import Command
 from aiogram.dispatcher.filters import Text
 from config_reader import config
-
-import asyncio
-import logging
-
 from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
+from coordinates import get_coordinates
+from weather_api_service import get_weather
 
 load_dotenv()
 
@@ -16,7 +18,7 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
 dp = Dispatcher(bot)
-URL = 'https://randomfox.ca/floof/'
+URL_1 = 'https://randomfox.ca/floof/'
 
 
 @dp.message_handler(Command("start"))
@@ -36,23 +38,45 @@ async def cmd_start(message: types.Message):
     await message.answer("Выберите пункт меню:", reply_markup=keyboard)
 
 
+def get_new_image():
+    try:
+        response = requests.get(URL_1)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thecatapi.com/v1/images/search'
+        response = requests.get(new_url)
+        response = response.json()
+        random_cat = response[0].get('url')
+        return random_cat
+    response = response.json()
+    random_fox = response.get('image')
+    return random_fox
+
+
+def weather():
+    """Возвращает сообщение о температуре погоды."""
+    wthr = get_weather(get_coordinates())
+    return f'{wthr.location}, {wthr.description}\n' \
+           f'Temperature is {wthr.temperature}°C, feels like {wthr.temperature_feeling}°C'
+
+
 @dp.message_handler(Text("Прогноз погоды"))
-async def with_puree(message: types.Message):
-    await message.reply("Жара!")
+async def show_weather(message: types.Message):
+    await message.answer(text=weather())
 
 
 @dp.message_handler(Text("Конвертация валюты"))
-async def with_puree(message: types.Message):
+async def currency(message: types.Message):
     await message.reply("В рублях или в евро?")
 
 
-@dp.message_handler(Text("Пришли котика!"))
-async def cats(message: types.Message):
-    await bot.send_photo(message.chat.id, photo='https://yandex.ru/images/search?img_url=http%3A%2F%2Fon-desktop.com%2Fwps%2FAnimals___Cats_Red_Cat_with_open_mouth_044663_.jpg&lr=120226&pos=0&rpt=simage&source=serp&stype=image&text=rjn')
+@dp.message_handler(Text("Пришли лису!"))
+async def fox(message: types.Message):
+    await bot.send_photo(message.chat.id, get_new_image())
 
 
 @dp.message_handler(Text("Создай опрос"))
-async def without_puree(message: types.Message):
+async def poll(message: types.Message):
     await message.reply("Попозже!")
 
 
